8987afa0e1053af9205c2d97261e34d657807db4
ENOSISBD\mishu.baidya <mishu.baidya@enosisbd.com>
2024-11-25 19:55:11 +0600

    First commit

---
+++ my-vue-app/src/components/EmployeeForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Name</label>
+      <input v-model="employee.name" type="text" />
+      <label> Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+    methods: {
+    handleSubmit() {
+      console.log('testing handleSubmit')
+      this.$emit('add:employee', this.employee)
+    },
+  },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  input{
+    height: 6vh;
+    font-size: 18px;
+  }
+</style>
---
+++ my-vue-app/src/components/EmployeeTable.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="employee-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Employee name</th>
+            <th>Employee email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="employee in employees" :key="employee.id">
+            <td>{{ employee.name }}</td>
+            <td>{{ employee.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'employee-table',
+    props: {
+    employees: Array,
+    },
+}
+</script>
+  
+<style scoped>
+
+#employee-table {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#employee-table caption {
+  font-size: 1.3em;
+  margin: .5em 0 .75em;
+}
+
+#employee-table tr {
+  padding: .35em;
+}
+
+#employee-table th,
+#employee-table td {
+  color: white;
+  padding: .625em;
+  text-align: center;
+  border: 2px solid #ddd;
+}
+
+#employee-table th {
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+  
+}
+
+
+
+
+</style>
